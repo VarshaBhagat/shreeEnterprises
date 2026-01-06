5e6aaa0e12e459d894e1f78731ddfc6c7f281ffd
gourav jaiswal <gouravjaiswal@gouravs-MacBook-Pro.local>
2026-01-06 17:46:30 +0530

    version 0.0

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/shreeEnterprises/' // 👈 repo name
+  base: '/shreeEnterprises/',
+  build: {
+  outDir: 'dist',
+  emptyOutDir: true
+}
+
 })
